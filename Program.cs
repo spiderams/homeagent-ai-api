db447c0030be6a6fe2d8e633101183a48f97ee54
spiderams <ayanleh.moussasaid@gmail.com>
2026-05-19 21:50:13 -0400

    Auto run migrations on startup

--- Program.cs
+++ Program.cs
@@ -40,5 +40,11 @@ app.UseCors("AllowReact");
 app.UseAuthorization();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider
+        .GetRequiredService<ApplicationDbContext>();
 
+    db.Database.Migrate();
+}
 app.Run();

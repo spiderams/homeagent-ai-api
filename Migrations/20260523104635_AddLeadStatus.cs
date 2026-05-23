93febaaf4cb39cdc0582fb9824994b2702100c9c
Ayanleh Moussa Said <ayanleh_moussa.said@sct.gouv.qc.ca>
2026-05-23 06:50:04 -0400

    Ajout statut

---
+++ Migrations/20260523104635_AddLeadStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateAIAssistant.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLeadStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Leads",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Leads");
+        }
+    }
+}

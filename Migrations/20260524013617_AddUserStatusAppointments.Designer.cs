dd125b5edae3e2f6f235d650878ea299bcd7257e
Ayanleh Moussa Said <ayanleh_moussa.said@sct.gouv.qc.ca>
2026-05-23 21:36:47 -0400

    Added multi user leads column

--- Migrations/20260524013617_AddUserStatusAppointments.Designer.cs
+++ Migrations/20260524013617_AddUserStatusAppointments.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealEstateAIAssistant.Data;
 
@@ -10,9 +11,11 @@ using RealEstateAIAssistant.Data;
 namespace RealEstateAIAssistant.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260524013617_AddUserStatusAppointments")]
+    partial class AddUserStatusAppointments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.27");
---
+++ Migrations/20260524013617_AddUserStatusAppointments.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateAIAssistant.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserStatusAppointments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "ConversationMessages",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ConversationMessages");
+        }
+    }
+}
